2bf44ddab7894ccf076f5ffe558eb16bdb8d2434
Deen <dejimorenigbade@gmail.com>
2024-04-26 10:52:48 -0400

    add files

---
+++ Week1/README.md
@@ -0,0 +1,12 @@
+# AzureCloudopsChallenge
+
+Copied both repo
+copied the html and css file
+
+
+fixed issue by adding --origin-host-header
+add custom domain to script
+
+https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/windows-agent?view=azure-devops#permissions
+
+access roken to use all organizations
---
+++ Week1/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+RG="Deencloudop-rg"
+AccountName="cloudopsstaticstorage"
+ProfileName="static-cloudops-cdn"
+EndPoint="cloudopsendpoints"
+
+
+echo "--------------------------------------------------------"
+echo "|   Removing previous content from CDN edge locations  |"
+echo "--------------------------------------------------------"
+
+az cdn endpoint purge \
+      --name $EndPoint \
+      --profile-name $ProfileName \
+      --resource-group $RG \
+      --content-paths "/*"
+
+
+echo "--------------------------------------------------------"
+echo "|           Uploading to Azure Storage Account         |"
+echo "--------------------------------------------------------"
+
+Connection_String=$(az storage account show-connection-string --name $AccountName --resource-group $RG --query connectionString -o tsv)
+az storage blob upload-batch \
+  -d "\$web" \
+  -s "./web" \
+  --connection-string $Connection_String \
+  --overwrite \
+  --pattern "*"
